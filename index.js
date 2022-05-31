075c90fb1dbe40d6b7ff42ba6657843565f40d5b
Пользователь <simpla@ukr.net>
2022-05-31 20:01:18 +0300

    correct

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ import morgan from 'morgan';
 
 console.log("CORS", cors);
 const app = express()
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(morgan('dev'))
 
