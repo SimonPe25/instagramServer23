189e0d1d44e6d9671ebffc6bd6a9a85262611813
Пользователь <simpla@ukr.net>
2022-05-31 20:06:35 +0300

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import morgan from 'morgan';
 console.log("CORS", cors);
 const app = express()
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://frontend-for-instagram.herokuapp.com/home', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
