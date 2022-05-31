00642dd94e3b9f335cdcbd55a17111952d19c1f3
Пользователь <simpla@ukr.net>
2022-05-31 12:19:18 +0300

    cors

--- index.js
+++ index.js
@@ -9,13 +9,30 @@ import morgan from 'morgan';
 const app = express()
 
 app.use(morgan('dev'))
-app.use(cors({
-    exposedHeaders: '*'
-}))
+app.use(cors())
 
 const PORT = process.env.PORT || 5000
 const DB_URL = "mongodb+srv://simon:987654321@cluster0.8pv5z.mongodb.net/IntagramDB?retryWrites=true&w=majority"
 
+//Cors
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(express.json())
 app.use('/', router)
