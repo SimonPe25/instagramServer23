b995bea6398e558911c47ca821f7f4b432877a64
Пользователь <simpla@ukr.net>
2022-05-31 12:09:41 +0300

    make it bett99r

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './router.js';
 import cors from 'cors'
-import morgan from 'morgan'
+import morgan from 'morgan';
+
 
 
 const app = express()
 
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors({
+    exposedHeaders: '*'
+}))
 
 const PORT = process.env.PORT || 5000
 const DB_URL = "mongodb+srv://simon:987654321@cluster0.8pv5z.mongodb.net/IntagramDB?retryWrites=true&w=majority"
