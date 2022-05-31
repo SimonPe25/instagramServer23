28fe0f1fcca4e70d012d0e0269b97e438000ca9a
Пользователь <simpla@ukr.net>
2022-05-31 21:02:35 +0300

    d

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ import router from './router.js';
 import cors from 'cors'
 import morgan from 'morgan';
 
-console.log("CORS", cors);
+
 const app = express()
-const corsOptions ={
-    origin:'https://frontend-for-instagram.herokuapp.com/home', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+// const corsOptions ={
+//     origin:'http://localhost:3000/home', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// } 
+// app.use(cors(corsOptions));
 
 app.use(morgan('dev'))
 
