feff4c0e7a8e1f6f48ac2146022cc084ddbe9c70
Simon <simpla@ukr.net>
2023-02-09 01:50:50 +0200

    first commit

--- router.js
+++ router.js
@@ -3,14 +3,14 @@ import PostController from "./PostControllers.js";
 
 const router = new Router()
 
-router.get('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.send('Hello World');
-  })
+// router.get('/', function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.send('Hello World');
+//   })
   
-  app.listen(3000, function () {
-    console.log('alive');
-  })
+//   app.listen(3000, function () {
+//     console.log('alive');
+//   })
 
 router.put('/users', PostController.update)
 router.get('/users/:id', PostController.getOne)
